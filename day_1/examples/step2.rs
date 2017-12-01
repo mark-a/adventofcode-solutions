3d9d483690998d7d223a66a01822259e4f057692
Mark Andrae <ma@protel.net>
2017-12-01 12:17:29 +0100

    added day1 step 2

---
+++ day_1/examples/step2.rs
@@ -0,0 +1,27 @@
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+
+fn main() {
+    let input_file = BufReader::new(File::open("input").unwrap());
+    let mut input_lines: Vec<_> = input_file.lines().map(|line| { line.unwrap() }).collect();
+    assert!(input_lines.len() > 0, "empty input");
+
+    let workset: Vec<_> = input_lines[0].split("").filter_map(|n| n.parse::<i32>().ok()).collect();
+    let mut sum = 0;
+    for (i,digit) in workset.iter().enumerate() {
+    	let mut next: usize = (i + workset.len() / 2) as usize; 
+		
+	if next > workset.len() - 1 {
+	  next = next - workset.len();
+	}
+
+	if *digit == workset[next] {
+	        println!("adding number at index {} because {} = {}", i, *digit, workset[next]);
+		sum = sum + *digit;
+	}
+
+    }
+
+    println!("the solution is {}", sum);
+
+}
--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,27 +1,3 @@
-use std::io::{BufRead, BufReader};
-use std::fs::File;
-
-fn main() {
-    let input_file = BufReader::new(File::open("input").unwrap());
-    let mut input_lines: Vec<_> = input_file.lines().map(|line| { line.unwrap() }).collect();
-    assert!(input_lines.len() > 0, "empty input");
-
-    let workset: Vec<_> = input_lines[0].split("").filter_map(|n| n.parse::<i32>().ok()).collect();
-    let mut sum = 0;
-    for (i,digit) in workset.iter().enumerate() {
-    	let next: usize = if i < workset.len() - 1 {
-	  (i+1)  as usize
-	} else{
-	  0 as usize
-	};
-
-	if *digit == workset[next] {
-	        println!("adding number at index {} because {} = {}", i, *digit, workset[next]);
-		sum = sum + *digit;
-	}
-
-    }
-
-    println!("the solution is {}", sum);
-
+fn main(){
+	println!("split in 2 steps, please run step1 and step 2 as examples ( cargo run --example step1 )")
 }
