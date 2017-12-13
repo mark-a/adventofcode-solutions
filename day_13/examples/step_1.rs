f78b7d4c17d1344ea22aa9071c9b54093da9804a
Mark Andrae <ma@protel.net>
2017-12-13 09:27:09 +0100

    day_13 starter... missing correct condition

---
+++ day_13/examples/step_1.rs
@@ -0,0 +1,31 @@
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+use std::collections::HashMap;
+
+fn main() {
+    let input_file = BufReader::new(File::open("input").unwrap());
+    let mut scanners = HashMap::new();
+    let mut highest = 0;
+    for io_line in input_file.lines() {
+        let line = io_line.unwrap();
+        let parts: Vec<_> = line.split(": ").collect();
+        let depth: i32 = parts[1].parse::<i32>().ok().unwrap();
+        let position: i32 = parts[0].parse::<i32>().ok().unwrap();
+        highest = position;
+        scanners.insert( position, depth);
+    }
+
+    let mut severity = 0;
+    for step in 0..highest+1 {
+        match scanners.get(step) {
+            Some(scanner_depth) => {
+                if step {
+                    severity += step* scanner_depth;
+                }
+            },
+            None => {}
+        };
+    }
+
+    println!("accumulated severity {}", severity);
+}
