523b36f7ed1596384f039201a05e248df5bca2b6
Mark Andrae <ma@neopoly.de>
2019-12-18 13:50:44 +0100

    add day 7 (2019)

---
+++ 2019/day_7/run_2.rb
@@ -0,0 +1,28 @@
+require_relative '../shared/intcode'
+
+filename = ARGV.first
+txt = open(filename)
+
+input = txt.read.strip.split(",").map(&:to_i)
+
+def chain(mem, phases)
+  amps = phases.map { |phase|
+    Intcode.new(mem).process(phase)
+  }
+
+  output = [0]
+  amps.each_with_index.cycle { |amp, id|
+    return output if amp.halted?
+    output = amp.process(output).output
+  }
+end
+
+
+range = (5..9)
+h = {}
+range.to_a.permutation { |phases|
+h[phases] = chain(input, phases).then { |xs|
+  xs[0]
+}
+}
+puts h.values.max
