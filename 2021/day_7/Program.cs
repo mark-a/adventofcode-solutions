30b96e243891979da635e5ee5019f8014cbf2e02
Mark Andrae <ma@neopoly.de>
2021-12-07 11:15:07 +0100

    Add day 7 of 21

---
+++ 2021/day_7/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] positions = File.ReadLines("input")
+    .First()
+    .Split(',')
+    .Select(x => Int32.Parse(x))
+    .ToArray();
+
+int[] deltas = new int[positions.Length];
+for (int i = 0; i < deltas.Length; i++)
+{
+    int deltaSum = 0;
+    for (int ii = 0; ii < positions.Length; ii++)
+    {
+        deltaSum += Math.Abs(positions[ii] - i);
+    }
+
+    deltas[i] = deltaSum;
+}
+
+Console.WriteLine("Stage 1: {0}",deltas.Min());
+
+
+
+int[] expDeltas = new int[positions.Length];
+for (int i = 0; i < expDeltas.Length; i++)
+{
+    int deltaSum = 0;
+    for (int ii = 0; ii < positions.Length; ii++)
+    {
+        int diff = Math.Abs(positions[ii] - i);
+        deltaSum +=   (1 + diff ) * diff / 2;
+    }
+
+    expDeltas[i] = deltaSum;
+}
+
+Console.WriteLine("Stage 2: {0}", expDeltas.Min());
