e3411eb2681f07001a755dbcfe4d955da7c03963
Mark Andrae <ma@neopoly.de>
2021-12-09 13:39:22 +0100

    Add day 8 and 9 of 2021

---
+++ 2021/day_8/Program.cs
@@ -0,0 +1,77 @@
+﻿string[] input = File.ReadAllLines("input");
+
+int count = input.Select(line => line.Split(" | ")[1])
+.SelectMany(l => l.Split())
+.Where(s => new[] { 2, 3, 4, 7 }.Contains(s.Length))
+.Count();
+
+Console.WriteLine("Stage 1: {0}", count);
+
+
+int outputSum = input.Select(InvestigateAndParse).Sum();
+Console.WriteLine("Stage 2: {0}", outputSum);
+
+static int InvestigateAndParse(string line)
+{
+	var split = line.Split(" | ");
+	var digits = split[0].Split();
+	var number = split[1].Split();
+
+	var one = digits.Single(x => x.Length == 2);
+	var four = digits.Single(x => x.Length == 4);
+	var seven = digits.Single(x => x.Length == 3);
+	var eight = digits.Single(x => x.Length == 7);
+
+	//deduct nine as 6 in length and only one field remaining when subtracting senven and four
+	var nine = digits.Single(x => x.Length == 6 && x.Except(seven).Except(four).Count() == 1);
+
+	// deduct six as 6 in length and not 9 and only one field remaining when subtracting from 1
+	var six = digits.Single(x => x.Length == 6	&& x != nine && one.Except(x).Count() == 1);
+
+	//  deduct zero as last 6 digit number
+	var zero = digits.Single(x => x.Length == 6 && x != nine && x != six);
+
+	// segment helper
+	var down_left = eight.Except(nine).Single();
+	var up_right = eight.Except(six).Single();
+	var down_right = one.Except(new[] { up_right  }).Single();
+
+	var five = digits.Single(x =>
+		x.Length == 5
+		&& !x.Contains(up_right)
+		&& !x.Contains(down_left));
+
+	// not five, but contains topright and not bottomright
+	var two = digits.Single(x =>
+		x.Length == 5
+		&& x != five
+		&& x.Contains(up_right)
+		&& !x.Contains(down_left));
+	// only five bar number left
+	var three = digits.Single(x =>
+		x.Length == 5
+		&& x != five
+		&& x != two);
+
+	// for easy search below
+	var numbers = new[]
+	{
+			(zero,0),
+			(one,1),
+			(two,2),
+			(three,3),
+			(four,4),
+			(five,5),
+			(six,6),
+			(seven,7),
+			(eight,8),
+			(nine,9)
+		};
+
+	return number
+		.Select(x => numbers.Where(n => n.Item1.Length == x.Length
+				&& !n.Item1.Except(x).Any()
+				&& !x.Except(n.Item1).Any())
+			.Single())
+		.Aggregate(0, (i, n) => i * 10 + n.Item2);
+}
---
+++ 2021/day_9/Program.cs
@@ -0,0 +1,94 @@
+﻿int[][] heightmap = File.ReadLines(@"input")
+    .Select(line => line.Select(x => Int32.Parse(x.ToString()))
+                    .ToArray())
+    .ToArray();
+
+List<(int, int, int)> localMinima = new List<(int, int, int)>();
+
+foreach (var (row, row_index) in heightmap.Select((item, index) => (item, index)))
+{
+    foreach (var (value, column_index) in row.Select((item, index) => (item, index)))
+    {
+        bool islocalMinima = true;
+        if (row_index > 0)
+        {
+            islocalMinima = islocalMinima && heightmap[row_index - 1][column_index] > value;
+        }
+        if (row_index < heightmap.Length - 1)
+        {
+            islocalMinima = islocalMinima && heightmap[row_index + 1][column_index] > value;
+        }
+        if (column_index > 0)
+        {
+            islocalMinima = islocalMinima && heightmap[row_index][column_index - 1] > value;
+        }
+        if (column_index < row.Length - 1)
+        {
+            islocalMinima = islocalMinima && heightmap[row_index][column_index + 1] > value;
+        }
+        if (islocalMinima)
+        {
+            localMinima.Add((value, row_index, column_index));
+        }
+    }
+}
+
+Console.WriteLine("Stage 1: {0}", localMinima.Select(x => x.Item1 + 1).Sum());
+
+
+var sizes = new List<long>();
+foreach (var (_value, row, column) in localMinima)
+{
+    var area = 0;
+    var seen = new HashSet<(int row, int column)>();
+
+    IEnumerable<(int row, int column)> traverse((int row, int column) p)
+    {
+        var (row, column) = p;
+
+        if (heightmap[row][column] == 9) yield break;
+
+        if (seen.Contains((row, column))) yield break;
+        seen.Add((row, column));
+
+        area++;
+
+        if (column > 0)
+            yield return (row, column - 1);
+        if (column < heightmap[row].Length - 1)
+            yield return (row, column + 1);
+        if (row > 0)
+            yield return (row - 1, column);
+        if (row < heightmap.Length - 1)
+            yield return (row + 1, column);
+    }
+
+    IEnumerable<T> BreadthFirst<T>(T root, Func<T, IEnumerable<T>> child)
+    {
+        return step();
+        IEnumerable<T> step()
+        {
+            Queue<T> queue = new Queue<T>();
+            queue.Enqueue(root);
+            while (queue.Count != 0)
+            {
+                T current = queue.Dequeue();
+                yield return current;
+                foreach (T item in child(current))
+                {
+                    queue.Enqueue(item);
+                }
+            }
+        }
+    }
+
+    foreach (var element in BreadthFirst((row, column), traverse)) ;
+
+    sizes.Add(area);
+}
+
+Console.WriteLine("Stage 2: {0}", sizes
+           .OrderByDescending(x => x)
+           .Take(3)
+           .Aggregate(1L, (a, b) => a * b)
+);
