013fb4bdfe19a443fc3750157af44f84ba80dc90
mark-a <major.sky@web.de>
2021-12-11 19:36:57 +0100

    use of fieldnames in tuples

--- 2021/day_11/Program.cs
+++ 2021/day_11/Program.cs
@@ -1,21 +1,21 @@
 ﻿string[] rows = File.ReadAllLines(@"input").ToArray();
 
-(int, bool)[,] energyMap = new (int, bool)[10, 10];
+(int value, bool flashed)[,] energyMap = new (int, bool)[10, 10];
 
 for (int i = 0; i < rows.Length; i++)
     for (int j = 0; j < rows[i].Length; j++)
     {
-        energyMap[j, i] = (Int32.Parse(rows[i][j].ToString()), false);
+        energyMap[j, i] = (value: Int32.Parse(rows[i][j].ToString()),flashed: false);
     }
 
 
-IEnumerable<((int, int), T)> Flatten<T>(T[,] map)
+IEnumerable<((int x, int y), T)> Flatten<T>(T[,] map)
 {
     for (int row = 0; row < map.GetLength(0); row++)
     {
         for (int col = 0; col < map.GetLength(1); col++)
         {
-            yield return ((row, col), map[row, col]);
+            yield return ((x: row, y: col), map[row, col]);
         }
     }
 }
@@ -28,22 +28,22 @@ while (true)
     for (int xx = 0; xx < rows.Length; xx++)
         for (int yy = 0; yy < rows.Length; yy++)
         {
-            energyMap[xx, yy].Item1 += 1;
+            energyMap[xx, yy].value += 1;
         }
 
     while (true)
     {
         try
         {
-            ((int, int), (int, bool)) nextElement = Flatten(energyMap).First(x => (x.Item2.Item1 > 9 && !x.Item2.Item2));
+            ((int x, int y) position, (int value, bool flashed) octopus) nextElement = Flatten(energyMap).First(x => (x.Item2.value > 9 && !x.Item2.flashed));
             energyMap[nextElement.Item1.Item1, nextElement.Item1.Item2].Item2 = true;
             flashes++;
-            for (int x = nextElement.Item1.Item1 - 1; x <= nextElement.Item1.Item1 + 1; x++)
-                for (int y = nextElement.Item1.Item2 - 1; y <= nextElement.Item1.Item2 + 1; y++)
+            for (int x = nextElement.position.x - 1; x <= nextElement.position.x + 1; x++)
+                for (int y = nextElement.position.y - 1; y <= nextElement.position.y + 1; y++)
                 {
                     if (x >= 0 && y >= 0 && x < rows.Length && y < rows.Length)
                     {
-                        energyMap[x, y].Item1++;
+                        energyMap[x, y].value++;
                     }
                 }
         }
@@ -55,14 +55,14 @@ while (true)
     }
 
 
-    if (Flatten(energyMap).All(x => x.Item2.Item2))
+    if (Flatten(energyMap).All(x => x.Item2.flashed))
     {
         break;
     }
     for (int x = 0; x < rows.Length; x++)
         for (int y = 0; y < rows.Length; y++)
         {
-            if (energyMap[x, y].Item2) energyMap[x, y] = (0, false);
+            if (energyMap[x, y].flashed) energyMap[x, y] = (value: 0, flashed: false);
         }
 
     if (counter == 100)
