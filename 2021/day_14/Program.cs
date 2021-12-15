ced7cd725b619f789d9e9ee7df1d03f6fb6477d3
Mark Andrae <ma@neopoly.de>
2021-12-15 11:54:01 +0100

    Add day 14 and 15 of 2021

---
+++ 2021/day_14/Program.cs
@@ -0,0 +1,58 @@
+﻿var parts = File.ReadAllText("input").Split("\n\n");
+
+var polymer = parts[0];
+
+var reactionDict = ReadLines(new StringReader(parts[1]))
+    .Select(line => line.Split(" -> "))
+    .Select(parts => (molecule: parts[0], element: parts[1]))
+    .ToDictionary(p => p.molecule, p => p.element);
+
+
+Console.WriteLine($"Stage 1: {loopAndCount(polymer, reactionDict, 10)}");
+
+Console.WriteLine($"Stage 2: {loopAndCount(polymer, reactionDict, 40)}");
+
+IEnumerable<string> ReadLines(StringReader reader)
+{
+    string line;
+    while ((line = reader.ReadLine()) != null)
+    {
+        yield return line;
+    }
+}
+
+long loopAndCount(string polymer, Dictionary<string, string> reactionDict, int cycles)
+{
+    var moleculeCount = new Dictionary<string, long>();
+    foreach (var i in Enumerable.Range(0, polymer.Length - 1))
+    {
+        var molecule = polymer.Substring(i, 2);
+        moleculeCount[molecule] = moleculeCount.GetValueOrDefault(molecule) + 1;
+    }
+
+    for (var i = 0; i < cycles; i++)
+    {
+        var updated = new Dictionary<string, long>();
+        foreach (var (molecule, count) in moleculeCount)
+        {
+            var (a, n, b) = (molecule[0], reactionDict[molecule], molecule[1]);
+            updated[$"{a}{n}"] = updated.GetValueOrDefault($"{a}{n}") + count;
+            updated[$"{n}{b}"] = updated.GetValueOrDefault($"{n}{b}") + count;
+        }
+        moleculeCount = updated;
+    }
+
+    var elementCounts = new Dictionary<char, long>();
+    foreach (var (molecule, count) in moleculeCount)
+    {
+        var a = molecule[0];
+        elementCounts[a] = elementCounts.GetValueOrDefault(a) + count;
+    }
+
+    elementCounts[polymer.Last()]++;
+
+    return elementCounts.Values.Max() - elementCounts.Values.Min();
+}
+
+
+
---
+++ 2021/day_15/Program.cs
@@ -0,0 +1,75 @@
+﻿var rows = File.ReadAllLines("input");
+
+int[][] riskMap = new int[rows.Length][];
+for (int i = 0; i < rows.Length; i++)
+{
+    riskMap[i] = new int[rows[i].Length];
+    for (int j = 0; j < rows[i].Length; j++)
+    {
+        riskMap[i][j] = (Int32.Parse(rows[i][j].ToString()));
+    }
+}
+
+var queue = new PriorityQueue<(int y, int x), int>();
+var total = new Dictionary<(int, int), int>();
+
+total[(0, 0)] = 0;
+queue.Enqueue((y: 0, x: 0), 0);
+
+while (queue.Count > 0)
+{
+    var current = queue.Dequeue();
+
+    foreach (var neighbor in new List<(int x, int y)> { (0, 1), (1, 0), (0, -1), (-1, 0) })
+    {
+        var x = current.x + neighbor.x;
+        var y = current.y + neighbor.y;
+        if (!total.ContainsKey((y, x)) && x >= 0 && y >= 0 && y < riskMap.Length && x < riskMap[0].Length)
+        {
+            var new_risk = total[(current.y, current.x)] + riskMap[y][x];
+            total[(y, x)] = new_risk;
+            queue.Enqueue((y, x), new_risk);
+        }
+    }
+
+}
+
+Console.WriteLine("Stage 1: {0}", total[(riskMap.Length - 1, riskMap[0].Length - 1)]);
+
+
+
+var queue2 = new PriorityQueue<(int y, int x), int>();
+var total2 = new Dictionary<(int, int), int>();
+
+total2[(0, 0)] = 0;
+queue2.Enqueue((y: 0, x: 0), 0);
+
+while (queue2.Count > 0)
+{
+    var current = queue2.Dequeue();
+
+    foreach (var neighbor in new List<(int x, int y)> { (0, 1), (1, 0), (0, -1), (-1, 0) })
+    {
+        var x = current.x + neighbor.x;
+        var y = current.y + neighbor.y;
+        if (!total2.ContainsKey((y, x)) && x >= 0 && y >= 0 && y < (riskMap.Length * 5) && x < (riskMap[0].Length * 5))
+        {
+            var new_risk = total2[(current.y, current.x)] + scaledRisk(y, x, riskMap);
+            total2[(y, x)] = new_risk;
+            queue2.Enqueue((y, x), new_risk);
+        }
+    }
+
+}
+
+
+Console.WriteLine("Stage 2: {0}", total2[((riskMap.Length * 5) - 1, (riskMap[0].Length * 5) - 1)]);
+
+int scaledRisk(int y, int x, int[][] original)
+{
+    int risk = original[y % original[0].Length][x % original.Length];
+    int addition = x / original.Length + y / original[0].Length;
+    risk += addition;
+    if (risk > 9) { risk -= 9; }
+    return risk;
+}
